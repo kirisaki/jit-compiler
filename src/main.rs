91c3f4937e2522dffa66e41dd2b7d8df9d6f4fda
Akihito KIRISAKI <kirisaki@klaraworks.net>
2021-08-30 10:18:12 +0900

    refactor: seperate the parser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ use std::{ffi::c_void, mem, ptr::{null_mut}};
 
 const PAGE_SIZE: usize = 1024 * 1024;
 
+#[derive(Clone, Debug)]
+enum Op{
+    Put,
+    Get,
+    IncVal,
+    DecVal,
+    IncPtr,
+    DecPtr,
+    Loop (Box<Vec<Op>>),
+}
+
 fn main() {
     let program: Box<[u8]> = compile(",+.".into());
     let f: fn(*mut u8) -> *mut u8;
@@ -40,17 +51,18 @@ unsafe fn allocate(len: usize) -> *mut c_void {
     q
 }
 
-fn compile(src: String) -> Box<[u8]> {
+fn compile(src: &str) -> Box<[u8]> {
     let mut program: Vec<u8> = vec!(
         // mov r15, rdi ; the current pointer at r15
         0x49, 0x89, 0xff,
         // mov rax, 0x0
         0x48, 0xc7, 0xc0, 0x00, 0x00, 0x00, 0x00,
     );
-    for c in src.as_str().chars() {
-        program.append(&mut match c {
-            // put a char
-            '.' => vec!(
+    let ops = parse(src).unwrap();
+    
+    for op in ops {
+        program.append(&mut match op {
+            Op::Put => vec!(
                 // mov rax, 0x01
                 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00,
                 // mov rdi, 0x01
@@ -62,8 +74,7 @@ fn compile(src: String) -> Box<[u8]> {
                 // syscall
                 0x0f, 0x05,
             ),
-            // read a char
-            ',' => vec!(
+            Op::Get => vec!(
                 // mov rax, 0x00
                 0x48, 0xc7, 0xc0, 0x00, 0x00, 0x00, 0x00,
                 // mov rdi, 0x00
@@ -75,8 +86,7 @@ fn compile(src: String) -> Box<[u8]> {
                 // syscall
                 0x0f, 0x05,
             ),
-            // increment the value pointed by r15
-            '+' => vec!(
+            Op::IncVal => vec!(
                 // mov ax, WORD PTR [r15]
                 0x66, 0x41, 0x8b, 0x07,
                 // inc ax
@@ -84,8 +94,7 @@ fn compile(src: String) -> Box<[u8]> {
                 // mov WORD PTR [r15], ax
                 0x66, 0x41, 0x89, 0x07,
             ),
-            // decrement the value pointed by r15
-            '-' => vec!(
+            Op::DecVal => vec!(
                 // mov ax, WORD PTR [r15]
                 0x66, 0x41, 0x8b, 0x07,
                 // dec ax
@@ -93,13 +102,11 @@ fn compile(src: String) -> Box<[u8]> {
                 // mov WORD PTR [r15], ax
                 0x66, 0x41, 0x89, 0x07,
             ),
-            // increment the pointer
-            '>' => vec!(
+            Op::IncPtr => vec!(
                 // inc r15
                 0x49, 0xff, 0xc7,
             ),
-            // decrement the pointer
-            '<' => vec!(
+            Op::DecPtr => vec!(
                 // dec r15
                 0x49, 0xff, 0xcf,
             ),
@@ -109,4 +116,38 @@ fn compile(src: String) -> Box<[u8]> {
     // program.append(&mut vec!(0x4c, 0x89, 0xf8));
     program.append(&mut vec!(0xc3)); // ret
     program.into_boxed_slice()
+}
+
+fn parse(src: &str) -> Result<Vec<Op>, &str> {
+    let mut rest: &str = src;
+    let mut ops: Vec<Op> = vec!();
+    loop {
+        if rest.len() == 0 {
+            return Ok(ops)
+        }
+        let (head, tail) = rest.split_at(1);
+        rest = tail;
+        match head {
+            "." => ops.push(Op::Put),
+            "," => ops.push(Op::Get),
+            "+" => ops.push(Op::IncVal),
+            "-" => ops.push(Op::DecVal),
+            ">" => ops.push(Op::IncPtr),
+            "<" => ops.push(Op::DecPtr),
+            "[" => {
+                match tail.rsplit_once("]") {
+                    None => return Err("mismatched brackets"),
+                    Some((l, r)) => {
+                        rest = r;
+                        match parse(l) {
+                            Ok(xs) => ops.push(Op::Loop(Box::new(xs))),
+                            Err(e) => return Err(e),
+                        }
+                    }
+                }
+            },
+            "]" => return Err("an unexpected bracket"),
+            _ => {},
+        }
+    }
 }
