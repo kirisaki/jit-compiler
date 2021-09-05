9b4fa805750ed53a4f3eb5c1f90c8a163c60fb82
Akihito KIRISAKI <kirisaki@klaraworks.net>
2021-09-05 22:34:10 +0900

    feat: enable loop

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,19 @@ enum Op{
 }
 
 fn main() {
-    let program: Box<[u8]> = compile(",+.".into());
-    let f: fn(*mut u8) -> *mut u8;
-    let res: *mut u8;
-    let p: *mut u8;
-    let m: *mut u8;
+    let program: Box<[u8]> = compile(">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++
+++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>
+++++++++[<++++>-]<+.[-]++++++++++.".into());
     unsafe {
+        let f: fn(*mut u8);
+        let p: *mut u8;
+        let m: *mut u8;
         p = allocate(program.len()) as *mut u8;
         m = allocate(PAGE_SIZE) as *mut u8;
-        println!("{:?} {:?}", p, m);
         p.copy_from(program.as_ptr(), program.len());
         f = mem::transmute(p);
+        f(m);
     }
-    res = f(m);
-    println!("\n{:?}", res);
 }
 
 unsafe fn allocate(len: usize) -> *mut c_void {
@@ -58,10 +57,17 @@ fn compile(src: &str) -> Box<[u8]> {
         // mov rax, 0x0
         0x48, 0xc7, 0xc0, 0x00, 0x00, 0x00, 0x00,
     );
-    let ops = parse(src).unwrap();
-    
+    let (ops, _) = parse(src, 0);
+    let mut asm = assemble(ops.unwrap());
+    program.append(&mut asm);    
+    program.append(&mut vec!(0xc3)); // ret
+    program.into_boxed_slice()
+}
+
+fn assemble(ops: Vec<Op>) -> Vec<u8> {
+    let mut output: Vec<u8> = vec!();
     for op in ops {
-        program.append(&mut match op {
+        output.append(&mut match op {
             Op::Put => vec!(
                 // mov rax, 0x01
                 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00,
@@ -79,28 +85,20 @@ fn compile(src: &str) -> Box<[u8]> {
                 0x48, 0xc7, 0xc0, 0x00, 0x00, 0x00, 0x00,
                 // mov rdi, 0x00
                 0x48, 0xc7, 0xc7, 0x00, 0x00, 0x00, 0x00,
-                // mov rdx, 0x01
-                0x48, 0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00,
                 // mov rsi, r15
                 0x4c, 0x89, 0xfe,
+                // mov rdx, 0x01
+                0x48, 0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00,
                 // syscall
                 0x0f, 0x05,
             ),
             Op::IncVal => vec!(
-                // mov ax, WORD PTR [r15]
-                0x66, 0x41, 0x8b, 0x07,
-                // inc ax
-                0x66, 0xff, 0xc0,
-                // mov WORD PTR [r15], ax
-                0x66, 0x41, 0x89, 0x07,
+                // inc BYTE PTR [r15]
+                0x41, 0xfe, 0x07,
             ),
             Op::DecVal => vec!(
-                // mov ax, WORD PTR [r15]
-                0x66, 0x41, 0x8b, 0x07,
-                // dec ax
-                0x66, 0xff, 0xc8,
-                // mov WORD PTR [r15], ax
-                0x66, 0x41, 0x89, 0x07,
+                // dec BYTE PTR [r15]
+                0x41, 0xfe, 0x0f,
             ),
             Op::IncPtr => vec!(
                 // inc r15
@@ -110,20 +108,50 @@ fn compile(src: &str) -> Box<[u8]> {
                 // dec r15
                 0x49, 0xff, 0xcf,
             ),
-            _ => vec!(),
+            Op::Loop(l) => {
+                let mut iter = assemble(l.to_vec());
+                let bs = (iter.len() as i32 + 12).to_le_bytes();
+                let cs = (-(iter.len() as i32) - 12).to_le_bytes();
+                let mut asm: Vec<u8> = vec!();
+                // the start of the loop
+                asm.append(&mut vec!(
+                    // mov al, BYTE PTR [r15]
+                    0x41, 0x8a, 0x07,
+                    // test al, al
+                    0x84, 0xc0,
+                    // jne <rel 0x05>
+                    0x75, 0x05,
+                    // jmp <rel offset>
+                    0xe9, bs[0], bs[1], bs[2], bs[3],
+                ));
+                // inside the loop
+                asm.append(&mut iter);
+                // the end of the loop
+                asm.append(&mut vec!(
+                    // mov al, BYTE PTR [r15]
+                    0x41, 0x8a, 0x07,
+                    // test al, al
+                    0x84, 0xc0,
+                    // je <rel 0x05>
+                    0x74, 0x05,
+                    // jmp <rel offset>
+                    0xe9, cs[0], cs[1], cs[2], cs[3],
+                ));
+                asm
+            },
         })
     }
-    // program.append(&mut vec!(0x4c, 0x89, 0xf8));
-    program.append(&mut vec!(0xc3)); // ret
-    program.into_boxed_slice()
+    output 
 }
 
-fn parse(src: &str) -> Result<Vec<Op>, &str> {
+fn parse(src: &str, depth: i64) -> (Result<Vec<Op>, &str>, &str) {
     let mut rest: &str = src;
     let mut ops: Vec<Op> = vec!();
     loop {
-        if rest.len() == 0 {
-            return Ok(ops)
+        if rest.len() == 0 && depth == 0 {
+            return (Ok(ops), "")
+        } else if rest.len() == 0 && depth > 0 {
+            return (Err("unbalanced bracket"), "")
         }
         let (head, tail) = rest.split_at(1);
         rest = tail;
@@ -135,18 +163,23 @@ fn parse(src: &str) -> Result<Vec<Op>, &str> {
             ">" => ops.push(Op::IncPtr),
             "<" => ops.push(Op::DecPtr),
             "[" => {
-                match tail.rsplit_once("]") {
-                    None => return Err("mismatched brackets"),
-                    Some((l, r)) => {
-                        rest = r;
-                        match parse(l) {
-                            Ok(xs) => ops.push(Op::Loop(Box::new(xs))),
-                            Err(e) => return Err(e),
-                        }
-                    }
+                match parse(tail, depth + 1) {
+                    (Ok(xs), ys) => {
+                        ops.push(Op::Loop(Box::new(xs)));
+                        rest = ys;
+                    },
+                    (Err(e), ys) => {
+                        return (Err(e), ys)
+                    },
                 }
             },
-            "]" => return Err("an unexpected bracket"),
+            "]" => {
+                if depth - 1 >= 0 {
+                    return (Ok(ops), rest)
+                } else {
+                    return (Err("unexpected bracket"), rest)
+                }
+            },
             _ => {},
         }
     }
